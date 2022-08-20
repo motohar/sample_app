cc4ab3f46e0f8a593212e1032d7b4c539c4aa897
EC2 Default User <110407718+motohar@users.noreply.github.com>
2022-08-20 06:36:57 +0000

    [Add]sample_app

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,16 @@
 class ListsController < ApplicationController
   def new
+    @list=List.new
   end
 
-  def index
-  end
+def create
+  list = List.new(list_params)
+  list.save
+  redirect_to'/top'
+end
 
-  def show
-  end
-
-  def edit
+  private
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
