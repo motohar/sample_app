82c134d04b50ff81c1d11f6aa67611e9f40580f6
EC2 Default User <110407718+motohar@users.noreply.github.com>
2022-08-27 09:02:15 +0000

    [Add]sample_app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'lists/new'
+  get 'lists/edit'
   get 'top' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
+  get 'lisys/:id' => 'lists#show'
+
 end
